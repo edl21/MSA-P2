416ebb861cdd43e91d999ad5d5980ba1b3be05a2
eli384 <88353612+edl21@users.noreply.github.com>
2023-08-01 17:48:53 +1200

    Login working

--- bodyhub/src/components/LoginForm.tsx
+++ bodyhub/src/components/LoginForm.tsx
@@ -19,34 +19,39 @@ const LoginForm: React.FC = () => {
       setValues({ ...values, [prop]: event.target.value });
     };
 
-    const handleSubmit = async (event: React.FormEvent) => {
-      event.preventDefault();
-    
-      // Email validation
-      const re = /\S+@\S+\.\S+/;
-      if (!re.test(values.email)) {
-        alert("Please enter a valid email");
-        return;
-      }
-    // GET request works "http://localhost:5127/api/User/" by calling it with the userid, I need to call it with the email so users can login with their email and password.
-      const queryParams = new URLSearchParams(values);
-      const url = `http://localhost:5127/api/User/Login?${queryParams.toString()}`;
-    
-      // Call your API to verify the credentials
-      const response = await fetch(url);
-    
-      if (response.status === 200) {
-        // Assume the login was successful
-        setOpen(true);
-        setTimeout(() => {
-          setOpen(false);
-          navigate("/"); // Redirect to home page or dashboard
-        }, 2000);
-      } else {
-        setError("Invalid email or password.");
-      }
-    };
-    
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    // Email validation
+    const re = /\S+@\S+\.\S+/;
+    if (!re.test(values.email)) {
+      alert("Please enter a valid email");
+      return;
+    }
+
+    const url = "http://localhost:5127/api/User/Login";
+
+    // Call your API to verify the credentials
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values), // Send the values as a JSON body
+    });
+
+    if (response.status === 200) {
+      // Assume the login was successful
+      setOpen(true);
+      setTimeout(() => {
+        setOpen(false);
+        navigate("/"); // Redirect to home page or dashboard
+      }, 2000);
+    } else {
+      setError("Invalid email or password.");
+    }
+  };
+
   const handleClose = (
     event: React.SyntheticEvent | Event,
     reason?: string

fc8c14d119ea594caae6eb792a7d3933e148f30c
eli384 <88353612+edl21@users.noreply.github.com>
2023-08-05 23:33:33 +1200

    More testing with deployment

--- p2api/Program.cs
+++ p2api/Program.cs
@@ -6,24 +6,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseInMemoryDatabase("AppData"));
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Enable middleware to serve static files
-app.UseStaticFiles();
-
-// Enable middleware to serve default files
-app.UseDefaultFiles();
-
 // Enable CORS
 app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 // Redirect HTTP to HTTPS
 app.UseHttpsRedirection();
+
+// Serve static files
+app.UseStaticFiles();  // <--- Add this line
 
 // Use routing
 app.UseRouting();
@@ -40,7 +36,5 @@ if (app.Environment.IsDevelopment())
 
 // Map the controllers
 app.MapControllers();
-
-app.MapFallbackToFile("index.html"); // Ensures unknown paths always serve index.html
 
 app.Run();
